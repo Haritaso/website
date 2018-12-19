889c46aa934f058b1a5fc645590e740d85d8e7f1
Haritaso <mm.620@icloud.com>
2018-12-19 19:31:50 +0900

    コンパクトに

--- src/components/backbord.vue
+++ src/components/backbord.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="back">
-    <svg xmlns="http://www.w3.org/2000/svg" x=0 y=0 width=400 height=180  font-size="40">
-      <rect x="0" y="0" width="400" height="180" rx="10" fill="#fff" fill-opacity="0.7" stroke-width="4" />
-      <image x="40" y="40" width="100" height="100" rx="10" xlink:href="https://github.com/haritaso.png" />
-      <rect x="38" y="38" width="104" height="104" rx="10" fill="none" fill-opacity="0" stroke="black" stroke-width="4"/>
+    <svg xmlns="http://www.w3.org/2000/svg" x=0 y=0 width=280 height=140  font-size="40">
+      <rect x="0" y="0" width="280" height="140" rx="10" fill="#fff" fill-opacity="0.7" stroke-width="4" />
+      <image x="20" y="30" width="80" height="80" rx="10" xlink:href="https://github.com/haritaso.png" />
+      <rect x="18" y="28" width="84" height="84" rx="10" fill="none" fill-opacity="0" stroke="black" stroke-width="4"/>
       <a href="https://haritaso.me">
-        <text x="180" y="100">Haritaso</text>
+        <text x="120" y="80">Haritaso</text>
       </a>
     </svg>
   </div>
