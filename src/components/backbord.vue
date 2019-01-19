09c22edf7e0b428480adc76f834a177874a7851a
Haritaso <mm.620@icloud.com>
2019-01-19 10:08:49 +0900

    update icon

--- src/components/backbord.vue
+++ src/components/backbord.vue
@@ -2,7 +2,7 @@
   <div class="back">
     <svg xmlns="http://www.w3.org/2000/svg" x=0 y=0 width=184 height=184  font-size="40">
       <rect x="0" y="0" width="184" height="184" rx="20" fill="#fff" fill-opacity="0.7" stroke-width="4" />
-      <image x="12" y="12" width="160" height="160" rx="10" xlink:href="../assets/icon.jpg" />
+      <image x="12" y="12" width="160" height="160" rx="10" xlink:href="../assets/icon2.jpg" />
       <rect x="10" y="10" width="164" height="164" rx="10" fill="none" fill-opacity="0" stroke="black" stroke-width="4"/>
     </svg>
   </div>
