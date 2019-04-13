604bf696e901ed73b7e21d87e7a6b876190fdc9e
Haritaso <mm.620@icloud.com>
2019-04-13 11:11:58 +0900

    import 修正

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { h, app } from 'hyperapp';
 import { withLogger } from '@hyperapp/logger';
 
-import Git from '@fortawesome/fontawesome-free-brands/faGithub';
-import Twitter from '@fortawesome/fontawesome-free-brands/faTwitter';
-import Mastodon from '@fortawesome/fontawesome-free-brands/faMastodon';
+import { faGithub, faTwitter, faMastodon } from '@fortawesome/fontawesome-free-brands';
 
 import GithubData from './actions/githubData';
 
@@ -35,13 +33,13 @@ const view = (state, actions) => (
   <div className={style.main}>
     <div className={style.iconGroup}>
       <a className={style.icon} href={'https://github.com/Haritaso'} onclick={actions.githubData.githubList}>
-        <Icon src={Git} className={style.awesomeIcon} />
+        <Icon src={faGithub} className={style.awesomeIcon} />
       </a>
       <a className={style.icon} href={'https://twitter.com/haritaso'}>
-        <Icon src={Twitter} className={style.awesomeIcon} />
+        <Icon src={faTwitter} className={style.awesomeIcon} />
       </a>
       <a className={style.icon} href={'https://tooting.ai/@hari_taso_'}>
-        <Icon src={Mastodon} className={style.awesomeIcon} />
+        <Icon src={faMastodon} className={style.awesomeIcon} />
       </a>
     </div>
     <div className={style.list}>
