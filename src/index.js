e4905065b36810e82e2d4bd4d6aeed71d1f5d62a
Haritaso <mm.620@icloud.com>
2019-04-13 19:57:45 +0900

    webpackのcss周り

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import '../style/style.scss';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>{count}</p>
+      <p className="aa">{count}</p>
       <button type="button" onClick={() => setCount(count + 1)}>a</button>
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+const WebpackBar = require('webpackbar');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
-  output: { filename: '[name].[hash].js', path: path.join(__dirname, 'dist') },
-  devtool: 'source-map',
+  output: { filename: '[hash].js', path: path.join(__dirname, 'dist') },
   module: {
     rules: [
       {
@@ -23,17 +27,55 @@ module.exports = {
         exclude: /node_modules/,
         use: [{ loader: 'eslint-loader' }],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: { reloadAll: true } },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader', options: { ident: 'postcss', plugins: () => postcssPresetEnv({ autoprefixer: { grid: true } }) } },
+          { loader: 'sass-loader' },
+        ],
+      },
     ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: 'index.html', // これ変えたらdevServer死ぬ
       template: 'public/index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new WebpackBar(),
   ],
   devServer: {
     port: 3000,
-    hot: true,
     stats: 'errors-only',
   },
 };
