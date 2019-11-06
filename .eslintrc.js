f8980299901eb8a86b7240bce2a5ab7c4755bd28
Haritaso <git@haritaso.me>
2019-11-06 20:51:55 +0900

    Delete all

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-const OFF = 0;
-
-module.exports = {
-  env: { browser: true },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  rules: {
-    'react/jsx-filename-extension': OFF
-  }
-};
--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  processors: ['stylelint-processor-styled-components'],
-  plugins: ['stylelint-order'],
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-styled-components',
-    'stylelint-config-prettier'
-  ]
-};
