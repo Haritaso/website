19c9e3250f998fabffa99c25ef7516e84d63acaf
Haritaso <git@haritaso.me>
2020-02-19 22:01:14 +0900

    Update App root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { h } from "preact"
-import { styled } from "linaria/react"
-
-const Text = styled.div`
-  color: red;
-`
+import { globals } from "./style/global"
+import Text from "./components/Text"
+import Twitter from "./components/Icon/Twitter"
+import Github from "./components/Icon/Github"
+import Icon from "./components/Icon"
+import Grid from "./components/Grid"
 
 const App = () => {
-  return <Text>aaa</Text>
+  return (
+    <div className={globals}>
+      <Text>aaa</Text>
+      <Grid alignItems="center">
+        <Icon size={120} compoent={<Twitter />} />
+        <Icon size={120} compoent={<Github />} />
+      </Grid>
+    </div>
+  )
 }
 
 export default App
