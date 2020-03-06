9c18ecccaf4947d29a7cef0235135094638c75d1
Haritaso <git@haritaso.me>
2020-03-06 19:14:02 +0900

    Add service worker

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,6 +1,8 @@
 import webpack from "webpack"
 import merge from "webpack-merge"
 import TerserPlugin from "terser-webpack-plugin"
+import WorkBoxWebpackPlugin from "workbox-webpack-plugin"
+import { resolve } from "path"
 import common from "./webpack.common"
 
 const config: webpack.Configuration = merge(common, {
@@ -11,6 +13,15 @@ const config: webpack.Configuration = merge(common, {
     ],
   },
   stats: "errors-only",
+  plugins: [
+    new WorkBoxWebpackPlugin.GenerateSW({
+      globDirectory: resolve(__dirname, "dist"),
+      globPatterns: ["*.{html,js,css}"],
+      swDest: resolve(__dirname, "dist/sw.js"),
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
 })
 
 export default config
